404418d429efb03afdfab36e4be2ff5477972f6a
Khushi Patel <khushipatel3340@gmail.com>
2023-08-12 23:39:23 +0530

    Added resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import './AboutMe.css'
+import { HiOutlineDownload } from "react-icons/hi";
+import { MdOpenInNew } from "react-icons/md"
+import { FaArrowRightLong } from "react-icons/fa6"
+import Resume from "../../public/assets/Khushi-Patel-resume.pdf"
 
 function AboutMe() {
   return (
     <div id='about' className='about'>
       <h1>About Me</h1>
-      <p className='about--info'>Hello World!! I'm a Computer Science student, absolutely captivated by web development, especially the fantastic realm of ReactJS. My passion revolves around taking those snazzy design concepts and turning them into actual functional websites. When I'm not diving into lines of code, you'll likely catch me reading books or sleeping. </p>
+      <p className="about--info">
+          <li>🎓 I'm a final year student pursuing Bachelors of Technology in Computer Engineering at Charusat University.</li>
+          <li>💼 Currently seeking opportunities in front-end development.</li>
+          <li>🌟 I have a strong interest in web development and user interfaces</li>
+          <li>🚀 Proficient in ReactJS and passionate about creating interactive web applications.</li>
+          <li>📚 Continuous learner with a keen interest in staying updated with the latest tech trends.</li>
+          <li>🌈 Enjoy creating intuitive and visually appealing user experiences.</li>
+          <li>🎮 Outside coding, I love watching anime, reading books and exploring new places.</li>
+      </p>
+      
+      <div className="about--button">  
+        <button className='about--button--resume'>
+          Resume 
+          <FaArrowRightLong size={25} style={{marginLeft:'10px'}}/>
+          <a href="https://drive.google.com/file/d/1LGu63_VfNlYjrHdx5E1DyjBlrAPe90XC/view?usp=sharing" target="_blank">
+          <button className='about--button--open'>
+            <MdOpenInNew size={30}/>
+          </button>
+          </a>
+          <a href={Resume} download="Khushi-Patel-resume.pdf">
+          <button className='about--button--download'>
+            <HiOutlineDownload size={30}/>
+          </button>
+          </a>
+        </button>
+      </div>
+      {/* <p className='about--info'>Hello World!! I'm a Computer Science student, absolutely captivated by web development, especially the fantastic realm of ReactJS. My passion revolves around taking those snazzy design concepts and turning them into actual functional websites. When I'm not diving into lines of code, you'll likely catch me reading books or sleeping. </p>
 
-      <p className='about--info'>This portfolio? Think of it as a scrapbook documenting my journey through the web development landscape. From quirky personal projects to my spotify playlist, it's all here. So take a little tour, explore around, and if you share the same enthusiasm for all things web, let's have a chat!</p>
+      <p className='about--info'>This portfolio? Think of it as a scrapbook documenting my journey through the web development landscape. From quirky personal projects to my spotify playlist, it's all here. So take a little tour, explore around, and if you share the same enthusiasm for all things web, let's have a chat!</p> */}
 
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,24 @@ import { HiMenu } from 'react-icons/hi'
 
 function Header() {
   const [showNavbar, setShowNavbar] = React.useState(false)
+  const [emoji, setEmoji] = React.useState("🍕")
 
   const handleShowNavbar = () => {
     setShowNavbar(!showNavbar);
   }
 
+  const emojis = ["🍕", "🍔", "🍟", "🍰", "🌼", "🌻", "🌹", "🍓", "🌞", "🌈", "🌟", "⛅", "🌎", "🪐", "🚀", "🥂", "🍉", "🌺", "🍁", "🧿", "⏳", "👧", "💅", "💀", "👽", "👾", "👀", "🐣", "🦢", "🦚", "🦋", "🦕", "🐸"]
+
+  function toggleEmojis() {
+    const randomEmoji = Math.floor(Math.random() * emojis.length)
+    setEmoji(emojis[randomEmoji])
+  }
+
   return (
     <nav className="navbar">
       <div className="container">
-        <h1>
-          <AnchorLink className='links' href='#home'>&lt;Khushi&gt;</AnchorLink>
+        <h1 onClick={toggleEmojis}>
+            &lt;Khushi {emoji}&gt;
         </h1>
         <div className="menu-icon" onClick={handleShowNavbar}>
           <HiMenu size={25}/>
